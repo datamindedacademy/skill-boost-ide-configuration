2494b608a20109d18061cb9a99a7f077bfc742db
Gergely Soti <gergely.soti@dataminded.be>
2025-04-08 14:19:21 +0200

    improvement for conditional breakpoint

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import pandas as pd
 from utils import dataframe_modifier
 
 a = 3
-b = 3-1
 
-print(a / b)
+for cnt in range(a+1):
+    b = a - cnt
+    print(a / b)
 
 df = pd.DataFrame([[1,2],[2,3]], columns=["a","b"])
 print(df)
 
-df = dataframe_modifier(df, b)
+df = dataframe_modifier(df, a)
 
 print(df)
